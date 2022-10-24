23e528b2436d37e21bc2bac51101082f8d9ab400
Nole Serrano <43283288+NoleSerrano@users.noreply.github.com>
2022-10-23 19:35:24 -0700

    Add files via upload

---
+++ CS 4350 PTS/src/MainMenu.java
@@ -0,0 +1,188 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class MainMenu extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainMenu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 710, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel titleLabel = new JLabel("Transit System");
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		titleLabel.setBounds(30, 11, 590, 17);
+		contentPane.add(titleLabel);
+
+		JButton recordData = new JButton("Record Data");
+		recordData.setBackground(SystemColor.controlHighlight);
+		recordData.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		recordData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		recordData.setBounds(30, 39, 140, 30);
+		recordData.setFocusPainted(false); // removes ugly focus
+		contentPane.add(recordData);
+
+		JButton weeklySchedule = new JButton("Weekly Schedule");
+		weeklySchedule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		weeklySchedule.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		weeklySchedule.setFocusPainted(false);
+		weeklySchedule.setBackground(SystemColor.controlHighlight);
+		weeklySchedule.setBounds(180, 39, 140, 30);
+		contentPane.add(weeklySchedule);
+
+		JButton displaySchedules = new JButton("Display Schedules");
+		displaySchedules.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		displaySchedules.setFocusPainted(false);
+		displaySchedules.setBackground(SystemColor.controlHighlight);
+		displaySchedules.setBounds(330, 39, 140, 30);
+		contentPane.add(displaySchedules);
+
+		JButton displayStops = new JButton("Display Stops");
+		displayStops.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		displayStops.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		displayStops.setFocusPainted(false);
+		displayStops.setBackground(SystemColor.controlHighlight);
+		displayStops.setBounds(480, 39, 140, 30);
+		contentPane.add(displayStops);
+
+		JButton driverButton = new JButton("Driver");
+		driverButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		driverButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		driverButton.setBackground(SystemColor.controlHighlight);
+		driverButton.setBounds(230, 80, 190, 30);
+		driverButton.setFocusPainted(false);
+		contentPane.add(driverButton);
+
+		JPopupMenu driverPM = new JPopupMenu();
+		driverPM.setLabel("");
+		addPopup(driverButton, driverPM);
+
+		JMenuItem addDr = new JMenuItem("Add");
+		driverPM.add(addDr);
+
+		JMenuItem deleteDr = new JMenuItem("Delete");
+		driverPM.add(deleteDr);
+
+		JMenuItem updateDr = new JMenuItem("Update");
+		driverPM.add(updateDr);
+
+		JMenuItem displayDr = new JMenuItem("Display");
+		driverPM.add(displayDr);
+
+		JButton tripOfferingButton = new JButton("Trip Offering");
+		tripOfferingButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		tripOfferingButton.setFocusPainted(false);
+		tripOfferingButton.setBackground(SystemColor.controlHighlight);
+		tripOfferingButton.setBounds(30, 80, 190, 30);
+		contentPane.add(tripOfferingButton);
+
+		JPopupMenu tripOfferingPM = new JPopupMenu();
+		addPopup(tripOfferingButton, tripOfferingPM);
+
+		JMenuItem addTO = new JMenuItem("Add");
+		tripOfferingPM.add(addTO);
+
+		JMenuItem deleteTO = new JMenuItem("Delete");
+		tripOfferingPM.add(deleteTO);
+
+		JMenuItem updateTO = new JMenuItem("Update");
+		tripOfferingPM.add(updateTO);
+
+		JMenuItem displayTO = new JMenuItem("Display");
+		tripOfferingPM.add(displayTO);
+
+		JButton busButton = new JButton("Bus");
+		busButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		busButton.setFocusPainted(false);
+		busButton.setBackground(SystemColor.controlHighlight);
+		busButton.setBounds(430, 80, 190, 30);
+		contentPane.add(busButton);
+
+		JPopupMenu busPM = new JPopupMenu();
+		addPopup(busButton, busPM);
+
+		JMenuItem addBu = new JMenuItem("Add");
+		busPM.add(addBu);
+
+		JMenuItem deleteBu = new JMenuItem("Delete");
+		busPM.add(deleteBu);
+
+		JMenuItem updateBu = new JMenuItem("Update");
+		busPM.add(updateBu);
+
+		JMenuItem displayBu = new JMenuItem("Display");
+		busPM.add(displayBu);
+	}
+	
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), popup.getX(), popup.getY() + 30);
+			}
+		});
+	}
+}

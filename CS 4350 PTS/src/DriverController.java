ae00c70c4bba69438120209dfef303901103e9be
Nole Serrano <noleserrano@yahoo.com>
2022-10-27 13:38:13 -0700

    Update DriverController.java

--- CS 4350 PTS/src/DriverController.java
+++ CS 4350 PTS/src/DriverController.java
@@ -51,7 +51,7 @@ public class DriverController {
 	public String[] getDriver(int driverID) {
 		try {
 			PreparedStatement stmt = con
-					.prepareStatement("SELECT DriverName, TelephoneNumber FROM Driver WHERE driverID=?");
+					.prepareStatement("SELECT DriverName, TelephoneNumber FROM Driver WHERE DriverID=?");
 			stmt.setInt(1, driverID);
 			ResultSet rs = stmt.executeQuery();
 			String[] driver = new String[2];
@@ -66,11 +66,42 @@ public class DriverController {
 	}
 
 	public void updateDriver(int driverID, String driverName, String telephoneNumber) {
-
+		try {
+			PreparedStatement stmt = con
+					.prepareStatement("UPDATE Driver SET DriverName=?, TelephoneNumber=? WHERE DriverID=?");
+			stmt.setString(1, driverName);
+			stmt.setString(2, telephoneNumber);
+			stmt.setInt(3, driverID);
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	public String[][] getDrivers() {
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM Driver");
 
+			ResultSet rs = stmt.executeQuery();
+			
+			rs.last(); // cursor at end
+			int rows = rs.getRow();
+			int cols = 3;
+			rs.beforeFirst(); // return cursor
+
+			String[][] drivers = new String[rows][cols];
+
+			int i = 0;
+			while (rs.next()) {
+				drivers[i][0] = String.valueOf(rs.getInt("DriverID"));
+				drivers[i][1] = rs.getString("DriverName");
+				drivers[i][2] = rs.getString("TelephoneNUmber");
+				i++;
+			}
+			return drivers;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		return null;
 	}
 }
